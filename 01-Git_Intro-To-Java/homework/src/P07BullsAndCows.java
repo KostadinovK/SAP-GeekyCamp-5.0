4585a6cc85826d893ba0bcda30baac63cc0f69bd
KostadinovK <kostadinovk2001@gmail.com>
2018-09-02 12:13:20 +0300

    Finished last task for 01 lection

--- 01-Git_Intro-To-Java/homework/src/P07BullsAndCows.java
+++ 01-Git_Intro-To-Java/homework/src/P07BullsAndCows.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -9,43 +10,7 @@ public class P07BullsAndCows {
         Scanner sc = new Scanner(System.in);
 
         printIntro();
-        int number = generate4DigitsNumberWithoutRepeatingDigits();
-        int guess = 0;
-        int tries = 0;
-        ArrayList<Integer> bullsAndCows = new ArrayList<>();
-        do{
-            System.out.println("Your guess(Should be four digits number without matching digits): ");
-            guess = sc.nextInt();
-        }while(isValidNumber(guess));
-
-        if(guess == number){
-            System.out.println("Congratulations, you won from the first time!");
-        }else{
-            bullsAndCows = howManyCowsAndBulls(guess,number);
-            System.out.printf("%d Bulls and %d Cows, Sorry try again!\n",bullsAndCows.get(0),bullsAndCows.get(1));
-            tries+=2;
-            while(true){
-                if(guess == 112){
-                    System.out.println("You have surrendered!");
-                    break;
-                }
-                do{
-                    System.out.printf("Your guess(Try %d)\n",tries);
-                    guess = sc.nextInt();
-                }while(isValidNumber(guess));
-
-                if(guess == number){
-                    System.out.printf("Four Bulls - Congratulations, you won from the %d time!\n",tries);
-                    break;
-                }else{
-
-                    bullsAndCows = howManyCowsAndBulls(guess,number);
-                    System.out.printf("%d Bulls and %d Cows\n",bullsAndCows.get(0),bullsAndCows.get(1));
-                    tries++;
-                }
-            }
-
-        }
+        playGame();
 
 
     }
@@ -74,62 +39,59 @@ public class P07BullsAndCows {
         System.out.println("You can always surrender by entering the surrender number - 112");
     }
 
-    public static int generate4DigitsNumberWithoutRepeatingDigits(){
 
-        int d1 = 0,d2 = 0, d3 = 0, d4 = 0;
+    public static void playGame(){
+        Random gen= new Random();
+        int num= 0;
+        while(hasDupes(num= (gen.nextInt(9000) + 1000)));
+        String numStr = num +"";
+        boolean guessed = false;
+        Scanner input = new Scanner(System.in);
+        int guesses = 0;
         do{
-            d1 = new Random().nextInt(9+1);
-            d2 = new Random().nextInt(9+1);
-            d3 = new Random().nextInt(9+1);
-            d4 = new Random().nextInt(9+1);
-        }while(d1 != d2 && d1 != d3 && d1 != d4 && d2 != d3 && d2 != d4 && d3 != d4);
-
-        String numStr = "" + d1 + "" + d2 + "" + d3 + "" + d4;
-
-        return Integer.parseInt(numStr);
-    }
-
-    public static boolean isValidNumber(int num){
-        if(num != 112){ //112 is the number for a surrender
-            if(num >= 1023 && num <= 9876){
-                String numStr = "" + num;
-                if(numStr.charAt(0) == numStr.charAt(1) || numStr.charAt(0) == numStr.charAt(2) || numStr.charAt(0) == numStr.charAt(3)
-                        || numStr.charAt(1) == numStr.charAt(2) || numStr.charAt(1) == numStr.charAt(3) || numStr.charAt(2) == numStr.charAt(3)){
-                    return true;
-                }else{
-                    return false;
+            int bulls = 0;
+            int cows = 0;
+            System.out.print("Guess a 4-digit number with no duplicate digits: ");
+            int guess;
+            try{
+                guess = input.nextInt();
+                if(guess == 112){
+                    System.out.println("You have surrendered, Game Over!");
+                    System.exit(0);
                 }
-            }else{
-                return false;
+                if(hasDupes(guess) || guess < 1000 || guess > 9999) continue;
+            }catch(InputMismatchException e){
+                continue;
             }
-        }else{
-            return true;
-        }
-
-    }
-
-    public static ArrayList<Integer> howManyCowsAndBulls(int guess,int number){
-        String guessStr = "" + guess;
-        String numStr = "" + number;
-        int bulls = 0;
-        int cows = 0;
-        for(int i = 0;i < guessStr.length();i++){
-            if(guessStr.charAt(i) == numStr.charAt(i)){
-                bulls++;
-            }
-        }
-
-        for(int i = 0;i < guessStr.length();i++){
-            for(int j = 0; j < guessStr.length();j++){
-                if(guessStr.charAt(i) == numStr.charAt(i) && i != j){
+            guesses++;
+            String guessStr = guess + "";
+            for(int i= 0;i < 4;i++){
+                if(guessStr.charAt(i) == numStr.charAt(i)){
+                    bulls++;
+                }else if(numStr.contains(guessStr.charAt(i)+"")){
                     cows++;
                 }
             }
-        }
-        ArrayList<Integer> res = new ArrayList<>();
-        res.add(bulls);
-        res.add(cows);
-        return res;
+            if(bulls == 4){
+                guessed = true;
+            }else{
+                System.out.println(cows+" Cows and "+bulls+" Bulls.");
+            }
+        }while(!guessed);
+        System.out.println("You won after "+guesses+" guesses!");
+
     }
+
+
+    public static boolean hasDupes(int num){
+        boolean[] digs = new boolean[10];
+        while(num > 0){
+            if(digs[num%10]) return true;
+            digs[num%10] = true;
+            num/= 10;
+        }
+        return false;
+    }
+
 
 }
